f67deba9a986f2e36457c2153a0d4d1f986e458b
Sergey Fedorenko <fedorenko_s_a@mail.ru>
2022-05-21 16:40:10 +0300

    feat: доработать централизованную обработку ошибок

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
 const {
-  getUsers, getUser, /* createUser, */ getUserMe, updateUser, updateAvatar,
+  getUsers, getUser, createUser, getUserMe, updateUser, updateAvatar,
 } = require('../controllers/users');
 
 router.get('/', getUsers);
 router.get('/:id', getUser);
 router.get('/me', getUserMe);
-// router.post('/', createUser);
+router.post('/', createUser);
 router.patch('/me', updateUser);
 router.patch('/me/avatar', updateAvatar);
 
