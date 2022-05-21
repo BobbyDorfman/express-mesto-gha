f623f35795c498c7fdc41e651cd5e14796ae6d99
Sergey Fedorenko <fedorenko_s_a@mail.ru>
2022-05-21 23:58:40 +0300

    feat: доработать валидицию данных на уровне схемы

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -1,6 +1,9 @@
 const { Joi, celebrate } = require('celebrate');
+const JoiObjectId = require('joi-objectid');
 
 const linkСhecking = /^((http|https):\/\/)?(www\.)?([A-Za-zА-Яа-я0-9]{1}[A-Za-zА-Яа-я0-9-]*\.?)*\.{1}[A-Za-zА-Яа-я0-9-]{2,8}(\/([\w#!:.?+=&%@!\-/])*)?/;
+
+Joi.objectId = JoiObjectId(Joi);
 
 const authorizationValidation = celebrate({
   body: Joi.object().keys({
@@ -34,7 +37,7 @@ const avatarValidation = celebrate({
 
 const userIdValidation = celebrate({
   params: Joi.object().keys({
-    id: Joi.string().required(),
+    id: Joi.objectId(),
   }),
 });
 
@@ -47,7 +50,7 @@ const cardValidation = celebrate({
 
 const cardIdValidation = celebrate({
   params: Joi.object().keys({
-    id: Joi.string().required(),
+    id: Joi.objectId(),
   }),
 });
 
